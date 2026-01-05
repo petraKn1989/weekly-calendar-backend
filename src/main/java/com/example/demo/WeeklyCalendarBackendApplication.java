69a4641326dd37da65f79d1b8e110036616e2dd3
petraKn1989 <knichalova@seznam.cz>
2026-01-05 11:05:27 +0100

    add cors for production

--- src/main/java/com/example/demo/WeeklyCalendarBackendApplication.java
+++ src/main/java/com/example/demo/WeeklyCalendarBackendApplication.java
@@ -9,17 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class WeeklyCalendarBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeeklyCalendarBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyCalendarBackendApplication.class, args);
+    }
 
-	@Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // všechny endpointy
-                        .allowedOrigins("http://localhost:4200") // tvůj frontend
+                registry.addMapping("/**") // všechny endpointy
+                        .allowedOrigins("http://localhost:4200",
+                                "https://weekly-calendar-frontend.vercel.app", 
+                                "https://www.tydenni-kalendar.cz") 
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
